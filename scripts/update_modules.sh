db2bab41a6fdb8c06ed1b3cdc15a2213caf0d7a8
vignesh <vignesh@loyalitsolutions.com>
2023-12-05 11:12:27 +0530

    sh

---
+++ scripts/update_modules.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+cd /home/ubuntu
+rm -rf UAA
+git clone -b production git@github.com:ajnaikka/sylcon-oddo.git
+
+sudo rm -fR /odoo/custom/addons/*
+sudo mv ~/sylcon-odoo/addons/* /odoo/custom/addons/
+
+sudo pip install -r /odoo/custom/addons/requirements.txt
+sudo service odoo-server restart 
