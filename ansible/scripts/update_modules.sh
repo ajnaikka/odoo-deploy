85b166959d809aaa68adcfd1a0d273c4340679d2
vignesh <vignesh@loyalitsolutions.com>
2023-12-05 12:07:00 +0530

    sh

--- ansible/scripts/update_modules.sh
+++ ansible/scripts/update_modules.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 cd /home/ubuntu
-rm -rf sylcon-odoo
+rm -rf sylcon-oddo
 git clone -b production git@github.com:ajnaikka/sylcon-oddo.git
 
 sudo rm -fR /odoo/custom/addons/*
-sudo mv ~/sylcon-odoo/addons/* /odoo/custom/addons/
+sudo mv ~/sylcon-oddo/addons/* /odoo/custom/addons/
 
-sudo pip install -r /odoo/custom/addons/requirements.txt
-sudo service odoo-server restart 
+#sudo pip install -r /odoo/custom/addons/requirements.txt
+#sudo service odoo-server restart 
